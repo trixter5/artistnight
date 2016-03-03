e2e67504973b3d7829a70305056aeb6c29ccac8f
trixter5 <trixter5@yandex.ru>
2016-03-03 05:09:09 +0300

    страница корпоративы: +corp.php, +corp_bg.jpg, /style.css, /header.php

---
+++ corp.php
@@ -0,0 +1,91 @@
+
+<?php include('header.php');?>
+
+<main class="w-section main main--mainpage">
+      <div class="article_title" style="background-image: url('http://nl.trixte.bget.ru/markup/images/corp_bg.jpg')">
+        <div class="w-container">
+          <h1 class="article_title--h1 corp_h1">тематические<br>корпоративы от<br><span class="h1-80">Artist<span class="article_title--h1red">night</span></span></h1>
+        </div>
+      </div>
+      <div class="content">
+        <div class="w-row content_row">
+          <div class="w-col w-col-4">
+            <div class="team_item corp_item">
+              <div class="corp_head">
+                <h3 class="team_h3 corp_h3">ЧАСТНЫЕ<br>ВЕЧЕРИНКИ И <br>КОРПОРАТИВЫ</h3>
+              </div>
+              <div class="team_par">
+                <ul class="corp_list">
+                  <li class="corp_li">
+                    <div class="corp_li--text">Отлично подойдут как для небольших, так и для многочисленных компаний.</div>
+                  </li>
+                  <li class="corp_li">
+                    <div class="corp_li--text">Проведение возможно на одной из наших площадок или на ваших собственных, можно даже в офисе!</div>
+                  </li>
+                  <li class="corp_li">
+                    <div class="corp_li--text">Дневные или вечерние мероприятия - на ваш выбор.</div>
+                  </li>
+                </ul>
+              </div>
+              <div class="corp_next">
+                <a href="#" class="w-inline-block corp_link">
+                  <div class="corp_next--text">выбрать</div>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="w-col w-col-4">
+            <div class="team_item corp_item">
+              <div class="corp_head">
+                <h3 class="team_h3 corp_h3">ФАНДРАЙЗИНГ</h3>
+              </div>
+              <div class="team_par">
+                <ul class="corp_list">
+                  <li class="corp_li">
+                    <div class="corp_li--text">Мы поможем собрать деньги на Ваши благие цели</div>
+                  </li>
+                  <li class="corp_li">
+                    <div class="corp_li--text">От 35 человек.</div>
+                  </li>
+                  <li class="corp_li">
+                    <div class="corp_li--text">Вы можете выбрать место проведения мероприятия, а также картину или тематику по вашему усмотрению.</div>
+                  </li>
+                </ul>
+              </div>
+              <div class="corp_next">
+                <a href="#" class="w-inline-block corp_link">
+                  <div class="corp_next--text">выбрать</div>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="w-col w-col-4">
+            <div class="team_item corp_item">
+              <div class="corp_head">
+                <h3 class="team_h3 corp_h3">ПОДАРОЧНАЯ <br>КАРТА</h3>
+              </div>
+              <div class="team_par">
+                <ul class="corp_list">
+                  <li class="corp_li">
+                    <div class="corp_li--text">Вы можете подарить билет на мероприятие в подарок вашим друзьям и близким.</div>
+                  </li>
+                  <li class="corp_li">
+                    <div class="corp_li--text">Подарочные билеты могут быть в виде красивого напечатанного билета или в электронном виде.</div>
+                  </li>
+                  <li class="corp_li">
+                    <div class="corp_li--text">Один подарочный билет дает право посетить мероприятие одному гостю.</div>
+                  </li>
+                </ul>
+              </div>
+              <div class="corp_next">
+                <a href="#" class="w-inline-block corp_link">
+                  <div class="corp_next--text">выбрать</div>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+    
+    <?php include('footer.php');?>
