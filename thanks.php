d78792ce9a413cb26a6e353441e769f15165d284
trixter5 <trixter5@yandex.ru>
2016-03-11 00:14:49 +0300

    add. thanks page

---
+++ thanks.php
@@ -0,0 +1,16 @@
+
+<?php include('header.php'); ?>
+
+    <main class="w-section main">
+      <div class="content content_simple">
+        <div class="eventpage_info thanks_page">
+          <div class="request_head">
+            <h1 class="request_h1">Завершено</h1>
+          </div>
+          <h2 class="thanks_h2">Спасибо за покупку!</h2>
+          <p class="thanks_p">This is Photoshop's version &nbsp;of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non &nbsp;mauris vitae erat consequat</p><img src="images/thanks_img.jpg" class="thanks_img"><a href="/" class="button thanks_btn">перейти на сайт</a>
+        </div>
+      </div>
+    </main>
+
+<?php include('footer.php'); ?>
